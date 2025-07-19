a227c40389edb82941660afeb843fac2262f482f
Jayesh Desale <jayeshdesale72@gamil.com>
2025-07-19 12:55:25 +0530

    your commit message

---
+++ js/product.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    id: 1,
+    name: "Smart Watch",
+    price: 1999,
+    image: "smartwatches.jpg"
+  },
+  {
+    id: 2,
+    name: "Bluetooth Speaker",
+    price: 1499,
+    image: "speakers.jpeg"
+  }
+];
+
+// Function to render on product.html or products.html
